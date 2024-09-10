3bd904189c74b0e578a61db5affa68443041805d
anastasia <hvorostok8@gmail.com>
2024-09-10 16:31:13 +0300

    3.Commit

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -274,8 +274,10 @@ class MandelbrotSetTask extends Task<Long> {
     private int calc(Complex comp) {
         int count = 0;
         Complex c = new Complex(0, 0);
+        Complex rand = new Complex(0.0001, 0);
         do {
             c = c.times(c).plus(comp);
+            c = c.plus(rand);
             count++;
         } while (count < CAL_MAX_COUNT-1 && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
